2c23f591ee9df66b331aaad6f521efe5b452f7d7
Guangrui Chen <gchen41@jhu.edu>
2019-09-18 04:09:43 -0400

    ncp response within timeout mostly implemented

--- exercises/ex1/ncp.c
+++ exercises/ex1/ncp.c
@@ -1,35 +1,10 @@
 #include "net_include.h"
+#include "packet.h"
 #include "sendto_dbg.h"
 
-#define WIN_SIZE 100
-#define PAC_SIZE 10
 #define NAME_LENGTH 80
 
 int main(int argc, char** argv) {
-  struct sockaddr_in name;
-  struct sockaddr_in send_addr;
-  struct sockaddr_in from_addr;
-  socklen_t from_len;
-  struct hostent h_ent;
-  struct hostent* p_h_ent;
-  char comp_name[NAME_LENGTH] = {'\0'};
-  char my_name[NAME_LENGTH] = {'\0'};
-  int host_num;
-  int from_ip;
-  int ss, sr;
-  fd_set mask;
-  fd_set read_mask, write_mask, excep_mask;
-  int bytes;
-  int num;
-  char mess_buf[MAX_MESS_LEN];
-  char dest_file_name[NAME_LENGTH] = {'\0'};
-
-  FILE* source_file;              // pointer to source file
-  char win[WIN_SIZE * PAC_SIZE];  // not sure about it
-  char pac[PAC_SIZE];
-  struct timeval timeout;
-  int num_read;
-
   // args error checking
   if (argc != 4) {
     printf(
@@ -38,12 +13,25 @@ int main(int argc, char** argv) {
     exit(0);
   }
 
-  // extract host name from args
+  char comp_name[NAME_LENGTH] = {'\0'};
+  char dest_file_name[NAME_LENGTH] = {'\0'};
+
+  int loss_rate_percent = atoi(argv[1]);
+  if ((loss_rate_percent <= 0 && argv[1] != "0") || loss_rate_percent > 100) {
+    perror("ncp: loss_rate_percent should be within range [0, 100]");
+    exit(1);
+  }
+
+  sendto_dbg_init(loss_rate_percent);
+
+  // extract computer name and destination file name from args
   char* temp = argv[3];
   int comp_char_index = 0;
   bool has_at = false;
   for (int i = 0; i < strlen(temp); i++) {
-    if ((!has_at) && temp[i] == '@') {
+    if ((!has_at) && temp[i] != '@') {
+      dest_file_name[i] = temp[i];
+    } else if ((!has_at) && temp[i] == '@') {
       has_at = true;
     } else if (has_at) {
       comp_name[comp_char_index++] = temp[i];
@@ -55,7 +43,26 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  // socket for receiving
+  struct sockaddr_in name;
+  struct sockaddr_in send_addr;
+  struct sockaddr_in from_addr;
+  socklen_t from_len;
+  struct hostent h_ent;
+  struct hostent* p_h_ent;
+  int host_num;
+  int from_ip;
+  int ss, sr;
+  fd_set mask;
+  fd_set read_mask, write_mask, excep_mask;
+  int bytes;
+  int num;
+  char my_name[NAME_LENGTH] = {'\0'};
+  // char mess_buf[MAX_MESS_LEN];
+
+  struct timeval timeout;
+  int num_read;
+
+  // socket for receiving messages
   sr = socket(AF_INET, SOCK_STREAM, 0);
   if (sr < 0) {
     perror("ncp: socket");
@@ -97,6 +104,7 @@ int main(int argc, char** argv) {
   FD_SET(sr, &mask);
   FD_SET((long)0, &mask); /* stdin */
 
+  FILE* source_file;  // pointer to source file
   if ((source_file = fopen(argv[2], "r")) == NULL) {
     perror("fopen");
     exit(0);
@@ -104,29 +112,127 @@ int main(int argc, char** argv) {
 
   printf("Opened %s for reading...\n", argv[2]);
 
-  for (;;) {
-    num_read = fread(pac, 1, PAC_SIZE, source_file);
+  struct packet_ack mess_ack_nack;
+  struct packet win[WINDOW_SIZE];
+  int cur_index;
+  char buffer[BUF_SIZE];
+  bool begin = false;
 
+  struct packet temp_pac;
+  // initialize window
+  for (int i = 0; i < WINDOW_SIZE; i++) {
+    temp_pac.tag = 1;
+    num_read = fread(buffer, 1, BUF_SIZE, source_file);
+    temp_pac.sequence = i;
+    for (int j = 0; j < BUF_SIZE; j++) {  // copy arr
+      temp_pac.file[j] = buffer[j];
+    }
+    win[i] = temp_pac;
+  }
+
+  for (;;) {
     read_mask = mask;
     timeout.tv_sec = 10;
     timeout.tv_usec = 0;
     num = select(FD_SETSIZE, &read_mask, &write_mask, &excep_mask, &timeout);
     if (num > 0) {
+      // receiving message
       if (FD_ISSET(sr, &read_mask)) {
         from_len = sizeof(from_addr);
-        bytes = recvfrom(sr, mess_buf, sizeof(mess_buf), 0,
+        bytes = recvfrom(sr, &mess_ack_nack, sizeof(mess_ack_nack), 0,
                          (struct sockaddr*)&from_addr, &from_len);
-        mess_buf[bytes] = 0;
-        from_ip = from_addr.sin_addr.s_addr;
+        // mess_buf[bytes] = 0;
 
-        printf("Received message from (%d.%d.%d.%d): %s\n",
+        from_ip = from_addr.sin_addr.s_addr;
+        printf("Received Ack and Nack message from (%d.%d.%d.%d): %s\n",
                (htonl(from_ip) & 0xff000000) >> 24,
                (htonl(from_ip) & 0x00ff0000) >> 16,
                (htonl(from_ip) & 0x0000ff00) >> 8,
-               (htonl(from_ip) & 0x000000ff), mess_buf);
+               (htonl(from_ip) & 0x000000ff), mess_ack_nack);
+
+        switch (mess_ack_nack.tag) {
+          // receiver is not busy, we can get started
+          case 0:
+            begin = true;
+            sendto_dbg(ss, (char*)&win[0], sizeof(struct packet), 0,
+                       (struct sockaddr*)&send_addr, sizeof(send_addr));
+            curr_ind = 1;
+            break;
+          // in the process of transferring packets.
+          // receives ack and nack
+          case 1:
+            unsigned char ack = mess_ack_nack.ack;
+            unsigned char nums_nack = mess_ack_nack.nums_nack;
+            unsigned int nack[WINDOW_SIZE] = mess_ack_nack.nack;
+
+            // slide window if ack is greater than first sequence
+            int first_sequence = win[0].sequence;
+            int last_sequence = win[WINDOW_SIZE].sequence;
+            int offset = ack - first_sequence;
+            // meaning that ack is different than before
+            if (offset >= 0) {
+              // copy the packets we need to keep to the front of the arr
+              for (int i = 0; i < offset + 1; i++) {
+                win[i] = win[i + offset + 1];
+              }
+              for (int j = last_sequence - offset; j < WINDOW_SIZE; j++) {
+                temp_pac.tag = 1;
+                num_read = fread(buffer, 1, BUF_SIZE, source_file);
+                if (num_read < BUF_SIZE) {
+                  if (feof(source_file)) {
+                    printf("Finished reading.\n");
+                    break;
+                  } else {
+                    printf("An error occurred when finishing reading\n");
+                    exit(0);
+                  }
+                }
+                temp_pac.sequence = offset + j + 1;
+                for (int k = 0; k < BUF_SIZE; k++) {  // copy arr
+                  temp_pac.file[k] = buffer[k];
+                }
+                win[j] = temp_pac;
+              }
+              // first element of nack is 0 indicating nack is empty??
+              if (nack[0] == 0) {  // nack is empty
+                sendto_dbg(ss, (char*)&win[curr_ind], sizeof(struct packet), 0,
+                           (struct sockaddr*)&send_addr, sizeof(send_addr));
+                curr_ind++;
+              } else {
+                for (int i = 0; i < sizeof(nack) / sizeof(nack[0]); i++) {
+                  if (nack[i] != 0) {
+                    sendto_dbg(ss, (char*)&win[nack[i] - first_sequence],
+                               sizeof(struct packet), 0,
+                               (struct sockaddr*)&send_addr, sizeof(send_addr));
+                  }
+                }
+              }
+            } else if (offset == -1 &&
+                       nack[0] != 0) {  // ack didn't change and there's nack
+              for (int i = 0; i < sizeof(nack) / sizeof(nack[0]); i++) {
+                if (nack[i] != 0) {
+                  sendto_dbg(ss, (char*)&win[nack[i] - first_sequence],
+                             sizeof(struct packet), 0,
+                             (struct sockaddr*)&send_addr, sizeof(send_addr));
+                }
+              }
+            } else {  // error otherwise
+              perror("ncp: ack error");
+              exit(1);
+            }
+
+            break;
+          // receiver is busy
+          case 2:
+            sleep(10);  // sleep for 10 seconds and then come back
+            break;
+          default:
+            perror("ncp: packet_ack tag error");
+            exit(1);
+        }
 
       } else if (FD_ISSET(0, &read_mask)) {
-        num_read = fread(pac, 1, PAC_SIZE, source_file);
+        num_read = fread(pac, 1, BUF_SIZE, source_file);
 
         if (num_read > 0) {  // if read something
           printf("There is an packet: %s\n", pac);
@@ -134,7 +240,7 @@ int main(int argc, char** argv) {
                      sizeof(send_addr));
         }
 
-        if (num_read < PAC_SIZE) {
+        if (num_read < BUF_SIZE) {
           /* Did we reach the EOF? */
           if (feof(source_file)) {
             printf("Finished writing.\n");
--- exercises/ex1/rcv.c
+++ exercises/ex1/rcv.c
@@ -1,163 +1,273 @@
 #include "net_include.h"
-#include "sendto_dbg.h"
 #include "packet.h"
+#include "sendto_dbg.h"
 
-unsigned int convert(unsigned int sequence, unsigned int start_sequence, unsigned int start_index);
+#define NACK_INTERVAL 2
+
+unsigned int convert(unsigned int sequence, unsigned int start_sequence,
+                     unsigned int start_index);
 
 int main(int argc, char** argv) {
-    
-    // argc erro checking
-    if (argc != 3) {
-        printf("Usage: rcv <loss_rate_percent>");
-        exit(0);
-    }
+  // argc error checking
+  if (argc != 3) {
+    printf("Usage: rcv <loss_rate_percent>");
+    exit(0);
+  }
 
-    int loss_rate_percent = atoi(argv[2]);
-    if (loss_rate_percent < 0 || loss_rate_percent > 100) {
-        printf("Warning: loss_rate_percent should be within range [0, 100]");
-    }
+  int loss_rate_percent = atoi(argv[2]);
+  if (loss_rate_percent < 0 || loss_rate_percent > 100) {
+    printf("Warning: loss_rate_percent should be within range [0, 100]");
+  }
 
-    sendto_dbg_init(loss_rate_percent);
+  sendto_dbg_init(loss_rate_percent);
 
-    bool busy = false;
+  bool busy = false;
 
-    // create a socket for receiving packets
-    int socket_rcv = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socket_rcv < 0) {
-        perror("Rcv: cannot create a socket for receiving");
-        exit(1);
-    }
+  // create a socket for receiving packets
+  int socket_rcv = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socket_rcv < 0) {
+    perror("Rcv: cannot create a socket for receiving");
+    exit(1);
+  }
 
-    struct sockaddr_in sockaddr_rcv;
-    sockaddr_rcv.sin_family = AF_INET;
-    sockaddr_rcv.sin_addr.s_addr = INADDR_ANY;
-    sockaddr_rcv.sin_port = htons(PORT);
+  struct sockaddr_in sockaddr_rcv;
+  sockaddr_rcv.sin_family = AF_INET;
+  sockaddr_rcv.sin_addr.s_addr = INADDR_ANY;
+  sockaddr_rcv.sin_port = htons(PORT);
 
-    // bind the socket with rcv address
-    if (bind(socket_rcv, (struct sockaddr*) &sockaddr_rcv, sizeof(sockaddr_rcv)) < 0) {
-        perror("Rcv: cannot bind socket with rcv address");
-        exit(1);
-    }
+  // bind the socket with rcv address
+  if (bind(socket_rcv, (struct sockaddr*)&sockaddr_rcv, sizeof(sockaddr_rcv)) <
+      0) {
+    perror("Rcv: cannot bind socket with rcv address");
+    exit(1);
+  }
 
-    int socket_sent = socket(AF_INET, SOCK_DGRAM, 0);
-    if (socket_sent < 0) {
-        perror("Rcv: cannot create a socket for sending");
-        exit(1);
-    }
+  int socket_sent = socket(AF_INET, SOCK_DGRAM, 0);
+  if (socket_sent < 0) {
+    perror("Rcv: cannot create a socket for sending");
+    exit(1);
+  }
 
-    fd_set read_mask;
-    fd_set mask;
-    FD_ZERO(&mask);
-    FD_SET(socket_rcv, &mask);
+  fd_set read_mask;
+  fd_set mask;
+  FD_ZERO(&mask);
+  FD_SET(socket_rcv, &mask);
 
-    struct timeval idle_interval;
+  // wait interval for connection requests
+  struct timeval idle_interval;
+  // interval in between sending NACK
+  struct timeval nack_interval;
+  nack_interval.tv_sec = NACK_INTERVAL;
+  nack_interval.tv_usec = 0;
 
-    struct sockaddr_in sockaddr_ncp;
-    socklen_t sockaddr_ncp_len;
+  struct sockaddr_in sockaddr_ncp;
+  socklen_t sockaddr_ncp_len;
 
-    struct packet packet_received;
-    struct packet packet_sent;
+  // packet received from sender
+  struct packet packet_received;
+  // packet delivered to sender
+  struct packet_ack packet_sent;
 
-    struct packet window[WINDOW_SIZE][BUF_SIZE];
-    // which sequence the first cell of window corresponds to
-    unsigned int start_sequence = 0;
-    // start index of the array
-    unsigned int start_index = 0;
-    // bool array to indicate if cell is filled
-    bool occupied[WINDOW_SIZE];
-    FILE *fw;
+  // window to store files
+  struct packet window[WINDOW_SIZE][BUF_SIZE];
+  // sequence which the first cell of window corresponds to
+  unsigned int start_sequence = 0;
+  // start index of the array
+  unsigned int start_index = 0;
+  // bool array to indicate if cell is filled
+  bool occupied[WINDOW_SIZE];
+  memset(occupied, 0, WINDOW_SIZE * sizeof(bool));
+  // timestamp array for NACK (initialized to zero)
+  struct timeval timestamps[WINDOW_SIZE];
+  for (int i = 0; i < WINDOW_SIZE; i++) {
+    timerclear(&timestamps[i]);
+  }
+  // file pointer for writing
+  FILE* fw;
 
-    for (;;) {
-        read_mask = mask;
-        idle_interval.tv_sec = 10;
-        idle_interval.tv_usec = 0;
-        int num = select(FD_SETSIZE, &read_mask, NULL, NULL, &idle_interval);
-        if (num > 0) {
-            if (FD_ISSET(socket_rcv, &read_mask)) {
-                sockaddr_ncp_len = sizeof(sockaddr_ncp);
-                // receive packet form ncp and store its address
-                int bytes = recvfrom(socket_rcv, &packet_received, sizeof(struct packet), 0,
-                        (struct sockaddr*) &sockaddr_ncp, &sockaddr_ncp_len);
-                int ncp_ip = sockaddr_ncp.sin_addr.s_addr;
-                switch (packet_received.tag) {
-                    // if sender wants to start transferring
-                    case 0:
-                        if (busy) {
-                            // send packet to notify busy
-                            packet_sent.tag = 2;
-                            printf("Sender (%d.%d.%d.%d) wants to connect, but busy\n",
-                                    (htonl(ncp_ip) & 0xff000000) >> 24,
-                                    (htonl(ncp_ip) & 0x00ff0000) >> 16,
-                                    (htonl(ncp_ip) & 0x0000ff00) >> 8,
-                                    (htonl(ncp_ip) & 0x000000ff));
-                        } else {
-                            // open or create file for writing
-                            char filename[BUF_SIZE];
-                            memcpy(filename, packet_received.file, packet_received.sequence);
-                            fw = fopen(filename, "w");
-                            if (fw == NULL) {
-                                perror("Rcv: cannot open or create file for writing");
-                                exit(1);
-                            }
-                            // send packet to start transfer
-                            packet_sent.tag = 0;
-                            printf("Establish connection with sender (%d.%d.%d.%d)\n",
-                                    (htonl(ncp_ip) & 0xff000000) >> 24,
-                                    (htonl(ncp_ip) & 0x00ff0000) >> 16,
-                                    (htonl(ncp_ip) & 0x0000ff00) >> 8,
-                                    (htonl(ncp_ip) & 0x000000ff));
-                        }
-                        sendto_dbg(socket_sent, &packet_sent, sizeof(struct packet), 0, 
-                                (struct sockaddr*) &sockaddr_ncp, sizeof(sockaddr_ncp));
-                        break;
-
-                    // if sender is trasferring
-                    case 1:
-                    {
-                        unsigned int index = convert(packet_received.sequence, start_sequence, start_index);
-                        // put buf in the corresponding spot in window
-                        memcpy(window[index], packet_received.file, BUF_SIZE);
-                        // mark cell as occupied
-                        occupied[index] = true;
-                        unsigned int new_start_index = start_index;
-
-                        // find the first gap in the window                        
-                        unsigned int cur;
-                        for (cur = start_sequence; cur < start_sequence + WINDOW_SIZE; cur++) {
-                            unsigned int actual_index = convert(cur, start_sequence, start_index);
-                            if (!occupied[actual_index]) {
-                                break;
-                            }
-                        }
-
-                        unsigned int gap = cur;
-                        // write to file and slide the window
-                        for (cur = start_sequence; cur < gap; cur++) {
-                            int bytes_written = fwrite(window[convert(cur, start_sequence, start_index)],
-                                    1, BUF_SIZE, fw);
-                        }
-                        
-
-                        break;
-                    }
-                    // if sender sends the last packet
-                    case 2:
-                        break;
-                }
-
-
+  for (;;) {
+    read_mask = mask;
+    idle_interval.tv_sec = 10;
+    idle_interval.tv_usec = 0;
+    int num = select(FD_SETSIZE, &read_mask, NULL, NULL, &idle_interval);
+    if (num > 0) {
+      if (FD_ISSET(socket_rcv, &read_mask)) {
+        sockaddr_ncp_len = sizeof(sockaddr_ncp);
+        // receive packet form ncp and store its address
+        int bytes =
+            recvfrom(socket_rcv, &packet_received, sizeof(struct packet), 0,
+                     (struct sockaddr*)&sockaddr_ncp, &sockaddr_ncp_len);
+        int ncp_ip = sockaddr_ncp.sin_addr.s_addr;
+        switch (packet_received.tag) {
+          // if sender wants to start transferring
+          case 0:
+            if (busy) {
+              // send packet to notify busy
+              packet_sent.tag = 2;
+              printf("Sender (%d.%d.%d.%d) wants to connect, but busy\n",
+                     (htonl(ncp_ip) & 0xff000000) >> 24,
+                     (htonl(ncp_ip) & 0x00ff0000) >> 16,
+                     (htonl(ncp_ip) & 0x0000ff00) >> 8,
+                     (htonl(ncp_ip) & 0x000000ff));
+            } else {
+              // open or create file for writing
+              char filename[BUF_SIZE];
+              memcpy(filename, packet_received.file, packet_received.sequence);
+              fw = fopen(filename, "w");
+              if (fw == NULL) {
+                perror("Rcv: cannot open or create file for writing");
+                exit(1);
+              }
+              // send packet to start transfer
+              packet_sent.tag = 0;
+              printf("Establish connection with sender (%d.%d.%d.%d)\n",
+                     (htonl(ncp_ip) & 0xff000000) >> 24,
+                     (htonl(ncp_ip) & 0x00ff0000) >> 16,
+                     (htonl(ncp_ip) & 0x0000ff00) >> 8,
+                     (htonl(ncp_ip) & 0x000000ff));
             }
-        } else {
-            printf(".");
-            fflush(0);
+            // TODO: packet_ack convert to char*
+            sendto_dbg(socket_sent, &packet_sent, sizeof(struct packet), 0,
+                       (struct sockaddr*)&sockaddr_ncp, sizeof(sockaddr_ncp));
+            break;
+
+          // if sender is trasferring
+          case 1: {
+            unsigned int index =
+                convert(packet_received.sequence, start_sequence, start_index);
+            // put buf in the corresponding spot in window
+            memcpy(window[index], packet_received.file, BUF_SIZE);
+            // mark cell as occupied
+            occupied[index] = true;
+            unsigned int new_start_index = start_index;
+
+            // find the first gap in the window
+            unsigned int cur;
+            for (cur = start_sequence; cur < start_sequence + WINDOW_SIZE;
+                 cur++) {
+              unsigned int actual_index =
+                  convert(cur, start_sequence, start_index);
+              if (!occupied[actual_index]) {
+                break;
+              }
+            }
+
+            unsigned int gap = cur;
+            // write to file
+            for (cur = start_sequence; cur < gap; cur++) {
+              int bytes_written =
+                  fwrite(window[convert(cur, start_sequence, start_index)], 1,
+                         BUF_SIZE, fw);
+              // clear the timestamp
+              timerclear(timestamps[convert(cur, start_sequence, start_index)]);
+            }
+
+            // put ACK in the return packet
+            packet_sent.tag = 1;
+            packet_sent.ack = gap - 1;
+
+            // determine if there is NACK
+            // if window is full
+            if (gap == start_sequence + WINDOW_SIZE) {
+              packet_sent.nums_nack = 0;
+            } else {
+              // find the last received packet in window
+              for (cur = start_sequence + WINDOW_SIZE - 1; cur >= gap; cur--) {
+                if (occupied[convert(cur, start_sequence, start_index)]) {
+                  break;
+                }
+              }
+
+              // if no received packet after gap
+              unsigned int last_received = cur;
+              if (last_received == gap - 1) {
+                packet_sent.nums_nack = 0;
+              } else {
+                int counter = 0;
+
+                struct timeval now;
+                struct timeval interval;
+
+                // find gaps in between
+                for (cur = gap; cur < last_received; cur++) {
+                  unsigned int actual_index =
+                      convert(cur, start_sequence, start_index);
+                  if (!occupied[actual_index]) {
+                    // if no timestamp, first NACK, include in packet
+                    if (!timerisset(timestamps[actual_index])) {
+                      packet_sent.nack[counter] = cur;
+                      counter++;
+                      // record current timestamp
+                      gettimeofday(&now, NULL);
+                      memcpy(timestamps[acutal_index], &now,
+                             sizeof(struct timeval));
+                    } else {
+                      gettimeofday(&now, NULL);
+                      timersub(&now, timestamps[actual_index], interval);
+                      // if interval is large, include in NACK
+                      if (!timercmp(&interval, &nack_interval, <)) {
+                        packet_sent.nack[counter] = cur;
+                        counter++;
+                        // record current timestamp
+                        gettimeofday(&now, NULL);
+                        memcpy(timestamps[acutal_index], &now,
+                               sizeof(struct timeval));
+                      }
+                    }
+                  }
+                }
+                packet_sent.nums_nack = counter;
+              }
+            }
+
+            // mark written cells as not occupied
+            for (cur = start_sequence; cur < gap; cur++) {
+              occupied[convert(cur, start_sequence, start_index)] = false;
+            }
+
+            // slide window
+            start_sequence = gap;
+            start_index = (start_index + gap - start_sequence) % WINDOW_SIZE;
+
+            // send packet back
+            // TODO: packet_ack convert to char*
+            sendto_dbg(socket_sent, &packet_sent, sizeof(struct packet), 0,
+                       (struct sockaddr*)&sockaddr_ncp, sizeof(sockaddr_ncp));
+            break;
+          }
+
+          // if sender sends the last packet
+          // TODO: test if first packet is last packet
+          case 2:
+
+            // clean up
+            fclose(fw);
+            busy = false;
+            start_sequence = 0;
+            start_index = 0;
+            memset(occupied, 0, WINDOW_SIZE * sizeof(bool));
+            for (int i = 0; i < WINDOW_SIZE; i++) {
+              timerclear(timestamps[i]);
+            }
+
+            printf("Finish trasnferring file with sender (%d.%d.%d.%d)\n",
+                   (htonl(ncp_ip) & 0xff000000) >> 24,
+                   (htonl(ncp_ip) & 0x00ff0000) >> 16,
+                   (htonl(ncp_ip) & 0x0000ff00) >> 8,
+                   (htonl(ncp_ip) & 0x000000ff));
+            break;
         }
-    
+      }
+    } else {
+      printf(".");
+      fflush(0);
     }
+  }
 
-
+  return 0;
 }
 
-
-unsigned int convert(unsigned int sequence, unsigned int start_sequence, unsigned int start_index) {
-    return (sequence - start_sequence + start_index) % WINDOW_SIZE;
-} 
+unsigned int convert(unsigned int sequence, unsigned int start_sequence,
+                     unsigned int start_index) {
+  return (sequence - start_sequence + start_index) % WINDOW_SIZE;
+}
