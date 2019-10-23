ada353fb9a7523abd4dc5cd8955161238883fb81
Minqi Ma <mma17@jhu.edu>
2019-10-22 20:01:51 -0400

    do not deliver current machine's packets; add back debugging methods

--- exercises/ex2/helper.c
+++ exercises/ex2/helper.c
@@ -25,9 +25,9 @@ struct timeval diffTime(struct timeval left, struct timeval right)
 
 
 unsigned int convert(unsigned int packet_index, unsigned int start_packet_index,
-                     unsigned int start_array_index)
+                     unsigned int start_array_index, int buffer_size)
 {
-  return (packet_index - start_packet_index + start_array_index) % WINDOW_SIZE;
+  return (packet_index - start_packet_index + start_array_index) % buffer_size;
 }
 
 bool check_finished_delivery(bool *finished, int * last_counters, int num_machines, int machine_index, int counter) {
@@ -51,4 +51,64 @@ bool check_acks(int * acks, int num_machines, int num_packets) {
         }
     }
     return min_ack == num_packets;
+}
+
+void print_status(int *acks, int *start_array_indices, 
+    int *start_packet_indices, int *end_indices, bool* finished, int *last_counters,
+    int counter, int last_delivered_counter, int num_created, int machine_index, int num_machines) {
+
+    printf("-------------------------------STATUS report-------------------------------\n");
+    // created packet
+    printf("Counter: %d\n", counter);
+    printf("----Created packets----\n");
+    printf("Created %d packets\n", num_created);
+    printf("start packet index: %d\n", start_packet_indices[machine_index - 1]);
+    printf("start array index: %d\n", start_array_indices[machine_index - 1]);
+    
+    // acks
+    printf("-------Ack-------\n");
+    for (int i = 0; i < num_machines; i++) {
+        printf("machine %d has delivered my packets up to %d\n", i + 1, acks[i]);
+    }
+
+    // table
+    printf("------Table------\n");
+    printf("Last delivered counter: %d\n", last_delivered_counter);
+    for (int i = 0; i < num_machines; i++) {
+        if (i + 1 != machine_index) {
+            printf("machine %d:\n", i + 1);
+            printf("start packet index: %d\n", start_packet_indices[i]);
+            printf("start array index: %d\n", start_array_indices[i]);
+        }
+    }
+
+    // end_indices
+    printf("------End indices------\n");
+    for (int i = 0; i < num_machines; i++) {
+        if (end_indices[i] == -1) {
+            printf("machine %d last packet index: unknown\n", i + 1);
+        } else {
+            printf("machine %d last packet index: %d\n", i + 1, end_indices[i]);
+        }
+    }
+
+    // finished
+    printf("------Finished------\n");
+    for (int i = 0; i < num_machines; i++) {
+        if (finished[i]) {
+            printf("machine %d: finished\n", i + 1);
+        } else {
+            printf("machine %d: not done\n", i + 1);
+        }
+    }
+
+    // last_counters 
+    printf("------Last Counters------\n");
+    for (int i = 0; i < num_machines; i++) {
+        printf("machine %d: last_counter is %d\n", i + 1, last_counters[i]);
+        
+    }
+
+    printf("------------------------------STATUS report END------------------------------\n\n");
+
 }
--- exercises/ex2/helper.h
+++ exercises/ex2/helper.h
@@ -9,7 +9,7 @@ struct timeval diffTime(struct timeval left, struct timeval right);
 /* Convert current sequence to corresponding index in the window
  * since smallest sequence starts at start_index
  */
-unsigned int convert(unsigned int sequence, unsigned int start_sequence, unsigned int start_index);
+unsigned int convert(unsigned int sequence, unsigned int start_sequence, unsigned int start_index, int buffer_size);
 
 /* Checks if my machine has finished delivering packets of all machines
  * if finished, update last_counter of this machine
@@ -19,6 +19,10 @@ bool check_finished_delivery(bool *finished, int * last_counters, int num_machin
 /* Checks if all other machines have finished delivering all my packets
  */
 bool check_acks(int * acks, int num_machines, int num_packets);
+
+void print_status(int *acks, int *start_array_indices, 
+    int *start_packet_indices, int *end_indices, bool* finished, int *last_counters,
+    int counter, int last_delivered_counter, int num_created, int machine_index, int num_machines);
 
 
 #endif
