bddb3cf09e7fbda7b54c74220e85eebca61fac58
Jerry <chen.gua@husky.neu.edu>
2019-11-22 20:51:11 -0500

    update Makefile

--- exercises/final/chat_server.c
+++ exercises/final/chat_server.c
@@ -170,6 +170,9 @@ static void receive_messages()
             SP_error(ret);
             exit(1);
         }
+
+
+
         if (Is_reg_memb_mess(service_type))
         {
             printf("Received REGULAR membership for group %s with %d members, where I am member %d:\n",
@@ -184,40 +187,19 @@ static void receive_messages()
 				printf("Due to the DISCONNECT of %s\n", memb_info.changed_member );
 			}else if( Is_caused_network_mess( service_type ) ){
 				printf("Due to NETWORK change with %u VS sets\n", memb_info.num_vs_sets);
-                                num_vs_sets = SP_get_vs_sets_info( mess, &vssets[0], MAX_VSSETS, &my_vsset_index );
-                                if (num_vs_sets < 0) {
-                                        printf("BUG: membership message has more then %d vs sets. Recompile with larger MAX_VSSETS\n", MAX_VSSETS);
-                                        SP_error( num_vs_sets );
-                                        exit( 1 );
-                                }
-                                for( i = 0; i < num_vs_sets; i++ )
-                                {
-                                        printf("%s VS set %d has %u members:\n",
-                                               (i  == my_vsset_index) ?
-                                               ("LOCAL") : ("OTHER"), i, vssets[i].num_members );
-                                        ret = SP_get_vs_set_members(mess, &vssets[i], members, MAX_MEMBERS);
-                                        if (ret < 0) {
-                                                printf("VS Set has more then %d members. Recompile with larger MAX_MEMBERS\n", MAX_MEMBERS);
-                                                SP_error( ret );
-                                                exit( 1 );
-                                        }
-                                        for( j = 0; j < vssets[i].num_members; j++ )
-                                                printf("\t%s\n", members[j] );
-                                }
-			}
-		}else if( Is_transition_mess(   service_type ) ) {
+		    }
+	    }else if( Is_transition_mess(   service_type ) ) {
 			printf("received TRANSITIONAL membership for group %s\n", sender );
 		}else if( Is_caused_leave_mess( service_type ) ){
 			printf("received membership message that left group %s\n", sender );
-		}else printf("received incorrecty membership message of type 0x%x\n", service_type );
-        } else if ( Is_reject_mess( service_type ) )
-        {
+		} else {printf("received incorrecty membership message of type 0x%x\n", service_type );}
+        
+    } else if ( Is_reject_mess( service_type ) ) {
 		printf("REJECTED message from %s, of servicetype 0x%x messtype %d, (endian %d) to %d groups \n(%d bytes): %s\n",
 			sender, service_type, mess_type, endian_mismatch, num_groups, ret, mess );
-	}else printf("received message of unknown message type 0x%x with ret %d\n", service_type, ret);
-
-
-            
-        }
+    }
+	
+    } else { 
+        printf("received message of unknown message type 0x%x with ret %d\n", service_type, ret); 
     }
 }
